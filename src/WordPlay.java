3bd1ea618b2b92a1237ae877a999c33ec40b1acc
John Buchanan Moore <johnbucmoore@gmail.com>
2019-12-05 11:56:20 -0500

    initial commit

---
+++ src/WordPlay.java
@@ -0,0 +1,56 @@
+public class WordPlay {
+
+    public boolean isVowel(char ch) {
+        String vowels = "aeiou";
+        if (vowels.indexOf(Character.toLowerCase(ch)) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+
+/*
+    public void digitTest() {
+        String test = "ABCabc0123456789';#!";
+        for (int k=0; k < test.length(); k++) {
+            char ch = test.charAt(k);
+            if (Character.isDigit(ch)) {
+                System.out.println(ch+" is a digit");
+            }
+            if (Character.isAlphabetic(ch)) {
+                System.out.println(ch+" is alphabetic");
+            }
+        }
+    }
+
+    public String encrypt(String input, int key) {
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String shiftedAlphabet = alphabet.substring(key)+alphabet.substring(0, key);
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(currChar);
+            if (idx != -1) {
+                char newChar = shiftedAlphabet.charAt(idx);
+                encrypted.setCharAt(i, newChar);
+            }
+        }
+        return encrypted.toString();
+    }
+*/
+    public void testIsVowel() {
+        System.out.println(isVowel('F'));
+        System.out.println(isVowel('g'));
+        System.out.println(isVowel('A'));
+        System.out.println(isVowel('!'));
+        System.out.println(isVowel('u'));
+        System.out.println(isVowel('9'));
+    }
+
+    public static void main(String[] args) {
+        WordPlay string = new WordPlay();
+        //string.digitTest();
+        //System.out.println(string.encrypt("Hello World!", 13));
+        string.testIsVowel();
+    }
+}
